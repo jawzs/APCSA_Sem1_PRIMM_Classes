4f4e4a96f2b523590262714f565fbaa23b0d325e
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-09-15 05:27:57 +0000

    Create ShapeDriver.java file.

---
+++ src/main/java/ShapeDriver.java
@@ -0,0 +1,38 @@
+/**
+ * Driver class for the shapes
+ * @author
+ * @version 1.0
+ * @since 2024-09
+ */
+
+
+class ShapeDriver {
+    public static void main(String args[]) {
+
+        Square sq1 = new Square();
+
+        System.out.printf("Side: %.2f\n", sq1.getSide());
+        // TODO: Print out perimeter, area
+
+
+        // TODO: Change the side length
+        // TODO: Print out side length, perimeter, area
+
+        // TODO: Change the perimeter
+        // TODO: Print out side length, perimeter, area
+
+        // TODO: Change the area
+        // TODO: Print out side length, perimeter, area
+
+        // TODO: After implementing a constructor that takes a side length
+        // create a new square with side length 4.5
+        // TODO: Print out side length, perimeter, area
+
+
+        // TODO: After implementing a new shape of your choosing
+
+
+    }
+
+
+}
