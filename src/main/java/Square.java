13a7435a08011d75164c16813b25d8cea73017c3
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-09-15 05:27:47 +0000

    Create Square.java file.

---
+++ src/main/java/Square.java
@@ -0,0 +1,67 @@
+/**
+ * Implements a class that encapsulates data and methods for representing a square
+ * @author 
+ * @version 1.0
+ * @since 2024-09-
+ */
+
+class Square {
+        /** The side length of the square **/
+    private double side = 1.0;
+
+        /** The perimeter of the square **/
+    private double perimeter = 4.0;
+
+        /** The area of the square **/
+        // TODO: add an instance variable for area
+    
+
+    /**
+     * Constructor
+     * @param side double representing the side length. Assumes positive non-zero 
+     */
+        // TODO: add a constructor that takes a side length
+
+
+    /**
+     * Gets side length
+     * @return double representing side
+     */
+    public double getSide() {
+        return side;
+    }
+
+    /**
+     * Gets perimeter
+     * @return double representing perimeter
+     */
+    // TODO: implement getter for perimeter
+
+    /**
+     * Gets area
+     * @return double representing area
+     */
+    // TODO: implement getter for area
+
+    /**
+     * Sets side length. Perimeter and area are updated
+     * @param s double representing the new side length.
+     */
+    public void setSide(double s) {
+        side = s;
+        perimeter = 4 * side; // calculate perimeter
+        // TODO: calculate the area
+    }
+
+    /**
+     * Sets area. Perimeter and side length are updated
+     */
+    // TODO: implement - you will need to import java.lang.Math to use sqrt() method
+
+
+    /**
+     * Sets perimeter. Area and side length are updated
+     */
+    // TODO: implement - you will need to import java.lang.Math to use sqrt() method
+
+}
